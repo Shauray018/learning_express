39e33a3436abc3bb2755116b952b57baf4453f61
Shauray Dhingra <shauraydhingra03@gmail.com>
2024-04-03 14:29:27 +0530

    corrected midwr

--- midwr.js
+++ midwr.js
@@ -1,36 +1,37 @@
-const express = require("express") 
-const app = express(); 
-app.use(express.json); 
+ const express = require("express");
+const app = express();
 
-app.get("/",aadhartaken,bloodpressure,bloodtest,function(req,res){
-    res.send("you are fine lil bro")
-})
+app.use(express.json());
 
-function aadhartaken(req,res,next) {
-    const havestuff = req.query.havestuff
- if ( havestuff = false) { 
-    res.send("if you're a broke boy just say so")
- } 
- else {next()}
-}
+app.get("/", aadhartaken, bloodpressure, bloodtest, function(req, res) {
+  res.json({ msg:"your fine"});
+});
 
-const bloodtest = (req,res,next) => {
-    const bt = req.query.bt
-    if (bt == "std") {
-        res.send("no checkup for whores")
+function aadhartaken(req, res, next) {
+    const havestuff = req.query.havestuff;
+    if (havestuff === "false") {
+        res.json({ msg:"if you're a broke boy just say so"});
     } else {
-        next() 
+        next();
     }
 }
 
-const bloodpressure = (req,res,next) => {
-    const bp = req.query.bp 
+function bloodtest(req, res, next) {
+    const bt = req.query.bt;
+    if (bt === "std") {
+      res.json({ msg:"it's not a whore house!!"});
+    } else {
+        next();
+    }
+}
+
+function bloodpressure(req, res, next) {
+    const bp = req.query.bp;
     if (bp < 120) {
-        res.send("bp too low, try again later")
-    } else{ 
-        next()
+      res.json({ msg:"bp too gohgh hg h"});
+    } else {
+        next();
     }
 }
 
-
-app.listen(3000); 
+app.listen(3000);
